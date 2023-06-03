a98eea0dab5603625628227caf2f11b800330fce
jcchouz <39292190+jcchouz@users.noreply.github.com>
2023-06-04 00:04:49 +0800

    20230604

--- leetcode/3. 无重复字符的最长子串.py
+++ leetcode/3. 无重复字符的最长子串.py
@@ -2,68 +2,22 @@ import collections
 
 
 class Solution:
-    def minWindow(self, s: 'str', t: 'str') -> 'str':
-        # 设置计数字典
-        lookup = collections.defaultdict(int)
-        # 存储每个目标字符的数量
-        for c in t:
-            lookup[c] -= 1
-        start, end = 0, 0
-        counter = -len(t)
-        min_len = float("inf")
-        res = ""
-        # end遍历
-        while end < len(s):
-            if lookup[s[end]] < 0:
-                counter += 1
-            lookup[s[end]] += 1
-            # 包含所有，start左移
-            while not counter:
-                if min_len > end - start + 1:
-                    min_len = end - start + 1
-                    res = s[start : end + 1]
-                if lookup[s[start]] == 0:
-                    counter -= 1
-                lookup[s[start]] -= 1
-                start += 1
-            end += 1
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s:
+            return 0
+        res = 0
+        # 创建滑动窗口，哈希表
+        hsmap = dict()
+
+        # 遍历判断是否重复
+        for i, c in enumerate(s):
+            hsmap.pop(next(iter(hsmap.keys())))
+            hsmap[i] = c
+            res = max(res, len(hsmap))
         return res
 
 
 if __name__ == '__main__':
     s = Solution()
     while True:
-        print(s.minWindow(eval(input()), eval(input())))
-
-'''
-class Solution:
-    def minWindow(self, s: 'str', t: 'str') -> 'str':
-        from collections import defaultdict
-        lookup = defaultdict(int)
-        for c in t:
-            lookup[c] += 1
-        start = 0
-        end = 0
-        min_len = float("inf")
-        counter = len(t)
-        res = ""
-        while end < len(s):
-            if lookup[s[end]] > 0:
-                counter -= 1
-            lookup[s[end]] -= 1
-            end += 1
-            while counter == 0:
-                if min_len > end - start:
-                    min_len = end - start
-                    res = s[start:end]
-                if lookup[s[start]] == 0:
-                    counter += 1
-                lookup[s[start]] += 1
-                start += 1
-        return res
-
-作者：powcai
-链接：https://leetcode.cn/problems/longest-substring-without-repeating-characters/solution/hua-dong-chuang-kou-by-powcai/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-'''
+        print(s.lengthOfLongestSubstring(eval(input())))
