e10c7961e106882a1eae8e0ddb8f87caaff545b8
jcchouz <39292190+jcchouz@users.noreply.github.com>
2023-06-03 15:02:46 +0800

    20230603

--- nowcoder_huaweijishi/HJ25. 数据分类处理.py
+++ nowcoder_huaweijishi/HJ25. 数据分类处理.py
@@ -1,107 +1,28 @@
-import math
-from typing import List
+import traceback
 
 
-class Solution:
-    def countPrimes(self, n: int) -> int:
-        if n < 3:
-            return 0
-        is_primes = [1] * n
-        is_primes[0] = is_primes[1] = 0
-        for i in range(2, int(n**0.5) + 1):
-            if is_primes[i] == 1:
-                is_primes[i**2 : n : i] = [0] & len(is_primes[i**2 : n : i])
-        return sum(is_primes)
+while True:
+    try:
+        I = input().split()[1:]
+        R = sorted(set(map(int, input().split()[1:])))
+        res = list()
+        for r in R:
+            r = str(r)
+            tmp = list()
+            for index, i in enumerate(I):
+                if r in i:
+                    tmp.extend([str(index), i])
+            if tmp:
+                res.extend([r, str(len(tmp) // 2)] + tmp)
+        print(str(len(res)) + ' ' + ' '.join(res))
 
-
-if __name__ == '__main__':
-    s = Solution()
-    print(s.countPrimes(2))
-
-
-'''
-        if n < 2: return 0
-        isPrimes = [1] * n
-        isPrimes[0] = isPrimes[1] = 0
-        for i in range(2, int(n ** 0.5) + 1):
-            if isPrimes[i] == 1:
-                isPrimes[i * i: n: i] = [0] * len(isPrimes[i * i: n: i])
-        return sum(isPrimes)
-
-作者：powcai
-链接：https://leetcode.cn/problems/count-primes/solution/qiu-zhi-shu-chao-guo-90-by-powcai/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-'''
+    except Exception as e:
+        print(traceback.format_exc())
+        break
 
 '''
-class Solution:
-    def countPrimes(self, n: int) -> int:
-        isPrimes = [1] * n
-        res = 0
-        for i in range(2, n):
-            if isPrimes[i] == 1: res += 1
-            j = i
-            while i * j < n:
-                isPrimes[i * j] = 0
-                j += 1
-        return res
+7 6396 4598 8539 6047 2019 11269 7402
+3 16 4 26
 
-作者：powcai
-链接：https://leetcode.cn/problems/count-primes/solution/qiu-zhi-shu-chao-guo-90-by-powcai/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-'''
-
-
-'''
-class Solution {
-public:
-    bool isPrime(int x) {
-        for (int i = 2; i * i <= x; ++i) {
-            if (x % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    int countPrimes(int n) {
-        int ans = 0;
-        for (int i = 2; i < n; ++i) {
-            ans += isPrime(i);
-        }
-        return ans;
-    }
-};
-
-作者：LeetCode-Solution
-链接：https://leetcode.cn/problems/count-primes/solution/ji-shu-zhi-shu-by-leetcode-solution/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-'''
-'''
-class Solution {
-public:
-    int countPrimes(int n) {
-        vector<int> isPrime(n, 1);
-        int ans = 0;
-        for (int i = 2; i < n; ++i) {
-            if (isPrime[i]) {
-                ans += 1;
-                if ((long long)i * i < n) {
-                    for (int j = i * i; j < n; j += i) {
-                        isPrime[j] = 0;
-                    }
-                }
-            }
-        }
-        return ans;
-    }
-};
-
-作者：LeetCode-Solution
-链接：https://leetcode.cn/problems/count-primes/solution/ji-shu-zhi-shu-by-leetcode-solution/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+12 4 3 1 4598 3 6047 6 7402 26 1 5 11269 
 '''
