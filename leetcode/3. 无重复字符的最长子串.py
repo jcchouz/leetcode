52be4e57209084b3a151b5ec69b18770b4ff25be
jcchouz <39292190+jcchouz@users.noreply.github.com>
2023-06-04 10:41:54 +0800

    20230604

--- leetcode/3. 无重复字符的最长子串.py
+++ leetcode/3. 无重复字符的最长子串.py
@@ -1,20 +1,36 @@
 import collections
 
 
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         if not s:
+#             return 0
+#         res = 0
+#         # 创建滑动窗口，哈希表
+#         hsmap = dict()
+#
+#         # 遍历判断是否重复
+#         for i, c in enumerate(s):
+#             hsmap.pop(next(iter(hsmap.keys())))
+#             hsmap[i] = c
+#             res = max(res, len(hsmap))
+#         return res
+
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if not s:
-            return 0
-        res = 0
-        # 创建滑动窗口，哈希表
-        hsmap = dict()
-
-        # 遍历判断是否重复
-        for i, c in enumerate(s):
-            hsmap.pop(next(iter(hsmap.keys())))
-            hsmap[i] = c
-            res = max(res, len(hsmap))
-        return res
+        # 滑动窗口，字典，计数
+        lookup = collections.defaultdict(int)
+        # end遍历
+        start = end = 0
+        max_len = 0
+        while end < len(s):
+            lookup[s[end]] += 1
+            while lookup[s[end]] > 1:
+                lookup[s[start]] -= 1
+                start += 1
+            max_len = max(max_len, end - start + 1)
+        return max_len
 
 
 if __name__ == '__main__':
--- leetcode/76. 最小覆盖子串.py
+++ leetcode/76. 最小覆盖子串.py
@@ -1,39 +1,35 @@
 import collections
 
 
-class Solution:
-    def minWindow(self, s: 'str', t: 'str') -> 'str':
-        # 设置计数字典
-        lookup = collections.defaultdict(int)
-        # 存储每个目标字符的数量
-        for c in t:
-            lookup[c] -= 1
-        start, end = 0, 0
-        counter = -len(t)
-        min_len = float("inf")
-        res = ""
-        # end遍历
-        while end < len(s):
-            if lookup[s[end]] < 0:
-                counter += 1
-            lookup[s[end]] += 1
-            # 包含所有，start左移
-            while not counter:
-                if min_len > end - start + 1:
-                    min_len = end - start + 1
-                    res = s[start : end + 1]
-                if lookup[s[start]] == 0:
-                    counter -= 1
-                lookup[s[start]] -= 1
-                start += 1
-            end += 1
-        return res
-
-
-if __name__ == '__main__':
-    s = Solution()
-    while True:
-        print(s.minWindow(eval(input()), eval(input())))
+# class Solution:
+#     def minWindow(self, s: 'str', t: 'str') -> 'str':
+#         # 设置计数字典
+#         lookup = collections.defaultdict(int)
+#         # 存储每个目标字符的数量
+#         for c in t:
+#             lookup[c] -= 1
+#         start, end = 0, 0
+#         counter = -len(t)
+#         min_len = float("inf")
+#         res = ""
+#         # end遍历
+#         while end < len(s):
+#             if lookup[s[end]] < 0:
+#                 counter += 1
+#             lookup[s[end]] += 1
+#             # 包含所有，start左移
+#             while not counter:
+#                 if min_len > end - start + 1:
+#                     min_len = end - start + 1
+#                     res = s[start : end + 1]
+#                 if lookup[s[start]] == 0:
+#                     counter -= 1
+#                 lookup[s[start]] -= 1
+#                 start += 1
+#             end += 1
+#         return res
+#
+#
 
 '''
 class Solution:
@@ -67,3 +63,37 @@ class Solution:
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 '''
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        # 滑动窗口，字典
+        lookup = collections.defaultdict(int)
+        # 每个目标字符单独计数
+        for c in t:
+            lookup[c] -= 1
+        # end遍历
+        start = 0
+        counter = len(t)
+        min_len = float("inf")
+        res = ""
+        for end, c in enumerate(s):
+            if lookup[c] < 0:
+                counter -= 1
+            lookup[c] += 1
+            # counter==0，找到所有
+            while not counter:
+                if min_len > end - start + 1:
+                    min_len = end - start + 1
+                    res = s[start : end + 1]
+                if not lookup[s[start]]:
+                    counter += 1
+                lookup[s[start]] -= 1
+                start += 1
+        return res
+
+
+if __name__ == '__main__':
+    s = Solution()
+    while True:
+        print(s.minWindow(eval(input()), eval(input())))
