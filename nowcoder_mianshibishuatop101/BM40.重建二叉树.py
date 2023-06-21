fc071952b3687c1927459f770f574e924d9e10aa
jcchouz <39292190+jcchouz@users.noreply.github.com>
2023-06-21 23:00:06 +0800

    20230621

--- nowcoder_mianshibishuatop101/BM40.重建二叉树.py
+++ nowcoder_mianshibishuatop101/BM40.重建二叉树.py
@@ -16,17 +16,43 @@ class TreeNode:
 # @param vinOrder int整型一维数组
 # @return TreeNode类
 #
-class Solution:
-    def reConstructBinaryTree(
-        self, preOrder: List[int], vinOrder: List[int]
-    ) -> TreeNode:
-        # write code here
-        if not preOrder:
-            return None
-        root = TreeNode(preOrder[0])
-        tmp = vinOrder.index(preOrder[0])
-        root.left = self.reConstructBinaryTree(preOrder[1 : tmp + 1], vinOrder[:tmp])
-        root.right = self.reConstructBinaryTree(
-            preOrder[tmp + 1 :], vinOrder[tmp + 1 :]
-        )
-        return root
+
+# 递归
+# class Solution:
+#     def reConstructBinaryTree(
+#         self, preOrder: List[int], vinOrder: List[int]
+#     ) -> TreeNode:
+#         # write code here
+#         if not preOrder:
+#             return None
+#         root = TreeNode(preOrder[0])
+#         tmp = vinOrder.index(preOrder[0])
+#         root.left = self.reConstructBinaryTree(preOrder[1 : tmp + 1], vinOrder[:tmp])
+#         root.right = self.reConstructBinaryTree(
+#             preOrder[tmp + 1 :], vinOrder[tmp + 1 :]
+#         )
+#         return root
+
+
+# 双指针
+# class Solution:
+#     def dfs(
+#         self,
+#         preStart: int,
+#         inStart: int,
+#         inEnd: int,
+#         preOrder: List[int],
+#         vinOrder: List[int],
+#     ) -> TreeNode:
+#         root = TreeNode(preOrder[0])
+#         index = vinOrder.index(preOrder[0])
+#         root.left = self.dfs(preStart + 1, inStart, index, preOrder, vinOrder)
+#         root.right = self.dfs(
+#             preStart + index - inStart + 1, index + 1, inEnd, preOrder, vinOrder
+#         )
+#         return root
+#
+#     def reConstructBinaryTree(
+#         self, preOrder: List[int], vinOrder: List[int]
+#     ) -> TreeNode:
+#         self.dfs(0, 0, len(vinOrder) - 1, preOrder, vinOrder)
